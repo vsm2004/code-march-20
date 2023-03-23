087160cbed55e4553f14997dae454b9e9e611eed
vsm2004 <128446657+vsm2004@users.noreply.github.com>
2023-03-23 11:38:47 +0530

    Add files via upload

---
+++ Untitled23.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    // Write C code here
+    int a=2,b=4;
+    printf("add(%d,%d)=%d\n",a,b,a+b);
+ printf("div(%d,%d)=%f\n",a,b,a%b);
+  printf("pow(%d,%d)=%d\n",a,b,pow(a,b));
+   printf("subt(%d,%d)=%d\n",a,b,a-b);
+    return 0;
+}
