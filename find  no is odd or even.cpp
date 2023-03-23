8f80576678874e1f05365a20f8f1accbdc30a269
vsm2004 <128446657+vsm2004@users.noreply.github.com>
2023-03-23 16:07:22 +0530

    Add files via upload

---
+++ find  no is odd or even.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int num;
+    printf("enter the number");
+    scanf("%d",&num);
+    if(num%2==0){
+        printf("%d is even",num);
+    }else{
+    printf("%d is odd",num);
+
+    return 0;
+}
+}
