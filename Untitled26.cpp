f7bac7b82a9596bae0a47a8bc32eae3bad8bc343
vsm2004 <128446657+vsm2004@users.noreply.github.com>
2023-03-23 14:25:00 +0530

    Add files via upload

--- Untitled26.cpp
+++ Untitled26.cpp
@@ -1,17 +1,8 @@
-#include<stdio.h>
-#include<math.h>
-int num;
-void square(){
-    int s=pow(num,2);
-    printf("%d\n",s);
-    cube();
-}
-void cube(){
-    int c=pow(num,3);
-    printf("%d\n",c);
-}
-int main(){
-    printf("enter the number: ");
-    scanf("%d",&num);
-    square();
+#include <stdio.h>
+
+int main() {
+    int i=5;
+    printf("%d\n%d\n%d\n%d\n%d\n%d",i++,i--,++i,--i,i);
+
+    return 0;
 }
