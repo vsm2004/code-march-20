c6cde1f5b283cde0438a4a795b00e814fd223be1
vsm2004 <128446657+vsm2004@users.noreply.github.com>
2023-03-23 14:36:10 +0530

    Add files via upload

---
+++ Untitled#.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	unsigned int num = -4;
+	printf("%d", ~num);
+	return 0;
+}
+
---
+++ Untitled@.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int a=(1 + 2 * 3 + 4 * 5);
+    printf("%d",a);
+
+    return 0;
+}
