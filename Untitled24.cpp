c12a750f40a5276af8be01b03b1d6733a0dc87b7
vsm2004 <128446657+vsm2004@users.noreply.github.com>
2023-03-23 13:48:29 +0530

    Add files via upload

---
+++ Untitled24.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+    int num, sum = 0, rem;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+ 
+    // Keep dividing until the number is not zero
+    while (num != 0)
+    {
+        rem = num % 10;
+        sum = sum + rem;
+        num = num / 10;
+    }
+    printf("Sum of digits of the number is %d", sum);
+    return 0;
+}
